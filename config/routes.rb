48877627a4eee6c7e93e462531faeca0e0c0f10c
yemelyn <pommy.shortcake@gmail.com>
2016-07-17 18:33:39 +0000

    edit posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'posts', to: 'posts#index'
   get 'posts/new', to: 'posts#new'
-  get 'posts/:id', to: 'posts#show'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  patch 'posts/:id', to: 'posts#update'
   post 'posts', to: 'posts#create'
-
-
 end
